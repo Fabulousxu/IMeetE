3f6e9df8950141197f9c21d4d6ad08f1e9823302
Fabulousxu <1292248830@qq.com>
2024-04-16 14:36:51 +0800

    上传了后端初始代码。
    1. 添加了《软件架构文档》
    2. 上传了后端初始代码。

---
+++ TechPrototype/IMeetEServer/src/main/java/org/example/imeeteserver/IMeetEServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.imeeteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMeetEServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IMeetEServerApplication.class, args);
+	}
+
+}
---
+++ TechPrototype/IMeetEServer/src/test/java/org/example/imeeteserver/IMeetEServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.imeeteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IMeetEServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
